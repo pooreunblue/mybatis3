f8a0ecefb56040843768f2b206ba7575945b06cb
bsh <mjbsh02@mju.ac.kr>
2026-07-23 09:23:28 +0900

    Initial commit

---
+++ src/test/java/org/example/mybatis3/Mybatis3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mybatis3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mybatis3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
